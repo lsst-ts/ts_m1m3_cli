1507eedcc5d2bf520b5db9928692424074e4c6ff
nsevilla <nsevilla@gmail.com>
2026-06-01 16:46:29 +0000

    Implemented patch from PR. Now time arguments are DurationTime format, hardpoints id can take ranges and string options such as all,odd,even, and outputs are on screen by default unless a filename is specified.

--- python/lsst/ts/m1m3/cli/m1m3_find_hp_excess.py
+++ python/lsst/ts/m1m3/cli/m1m3_find_hp_excess.py
@@ -21,12 +21,13 @@
 
 import argparse
 import asyncio
+import sys
 
 import pandas as pd
 from astropy.time import Time, TimeDelta
 from lsst_efd_client import EfdClient
 
-from lsst.ts.m1m3.utils import HPForces
+from lsst.ts.m1m3.utils import DurationTime, HPForces
 
 
 async def get_data(client: EfdClient, field: str, t_start: Time, t_end: Time) -> pd.DataFrame:
@@ -50,19 +51,28 @@ async def get_data(client: EfdClient, field: str, t_start: Time, t_end: Time) ->
 
 
 async def main() -> None:
+    now = Time.now()
+
     parser = argparse.ArgumentParser(description="Calculate HP excesses for a given definition of excess")
     parser.add_argument("--efd", default="usdf_efd", help="EFD name. Defaults to usdf_efd.")
     parser.add_argument(
-        "--t1",
-        type=str,
-        default="2025-12-30T00:00:00Z",
+        "start_time",
+        type=DurationTime(now),
+        default=now - TimeDelta(3600, format="sec"),
         help="Start time for excess check in a valid format: 'YYYY-MM-DDTHH:MM:SSZ'",
     )
     parser.add_argument(
-        "--t2",
-        type=str,
-        default="2025-12-30T00:59:59Z",
+        "end_time",
+        type=DurationTime(now),
+        default=now,
         help="End time for excess check in a valid format: 'YYYY-MM-DDTHH:MM:SSZ'",
+    )
+    parser.add_argument(
+        "hp_id",
+        choices=[str(i) for i in range(1, 7)] + ["all"] + ["odd"] + ["even"],
+        default=["all"],
+        action="append",
+        help="Hardpoint identifier [1-6], all, odd or even.",
     )
     parser.add_argument(
         "--delta_t",
@@ -71,43 +81,62 @@ async def main() -> None:
         help="Reference period (in seconds) in which an 'excess' is defined",
     )
     parser.add_argument(
-        "--delta_f_threshold",
+        "--delta-f-threshold",
         type=float,
         default=200,
         help="Minimum increment of force (in N) defining an excess (in delta_t)",
     )
     parser.add_argument(
-        "--time_gap_threshold",
+        "--time-gap-threshold",
         type=str,
         default="1s",
         help="Minimum time between events to consider a grouped excess. 1s is a good value.",
     )
     parser.add_argument(
-        "--hp_id",
-        type=int,
-        default=2,
-        help="Hardpoint identifier [1-6]",
-    )
-    parser.add_argument(
-        "--output_screen",
-        type=bool,
-        default=True,
-        help="Print out event summary on screen",
+        "--output",
+        type=str,
+        help="Save output to a given filename",
     )
     args = parser.parse_args()
     client = EfdClient(args.efd)
 
-    hp_index = args.hp_id - 1
-    df = await get_data(client, f"measuredForce{hp_index}", Time(args.t1), Time(args.t2))
-    sampling_freq = 20  # Hz
-    time_gap_threshold = TimeDelta(args.time_gap_threshold)
-    hpf = HPForces(df, sampling_freq, time_gap_threshold)
-    event_summary = hpf.calculate_excesses(args.hp_id, args.delta_t, args.delta_f_threshold)
-    output_filename = f"event_summary_HP{args.hp_id}_{args.t1}_{args.t2}.txt"
-    event_summary.to_csv(output_filename, sep="\t", index=False)
-    if args.output_screen:
-        print(event_summary)
-    print(f"Output saved to {output_filename}")
+    indexes = []
+
+    for hp_index in args.hp_id:
+        try:
+            indexes.append(int(hp_index) - 1)
+        except ValueError:
+            if hp_index == "all":
+                indexes = list(range(1, 7))
+            elif hp_index == "even":
+                indexes += list(range(2, 7, 2))
+            elif hp_index == "odd":
+                indexes += list(range(1, 7, 2))
+
+    indexes = sorted(set(indexes))
+
+    hp_excesses = []
+
+    sampling_freq = 50  # Hz
+
+    start_t, end_t = DurationTime.pair(args.start_time, args.end_time)
+
+    for hp_index in indexes:
+        df = await get_data(client, f"measuredForce{hp_index - 1}", start_t, end_t)
+        time_gap_threshold = TimeDelta(args.time_gap_threshold)
+        hpf = HPForces(df, sampling_freq, time_gap_threshold)
+        excess = hpf.calculate_excesses(hp_index, args.delta_t, args.delta_f_threshold)
+        excess["hp"] = hp_index
+        hp_excesses.append(excess)
+
+    event_summary = pd.concat(hp_excesses, ignore_index=True)
+    event_summary = event_summary.sort_values(by="start").reset_index(drop=True)
+
+    event_summary.to_csv(sys.stdout)
+
+    if args.output is not None:
+        event_summary.to_csv(args.output, index=False)
+        print(f"Output saved to {args.output}")
 
     if client.influx_client is None:
         await client._influx_client.close()
